a885837ce26caa35784c1c1a1e83e6febf29fd0c
Chroanz <christianhcoa@gmail.com>
2025-04-28 21:06:31 -0300

    feat: correção em rota

--- database/seeders/AulaSeeder.php
+++ database/seeders/AulaSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Aulas;
+use App\Models\Cursos;
 
 class AulaSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class AulaSeeder extends Seeder
      */
     public function run(): void
     {
-        Aulas::factory(30)->create();
+        // Get all courses
+        $cursos = Cursos::all();
+        
+        // Create aulas for each curso with sequential numbering
+        foreach ($cursos as $curso) {
+            // Create 5 aulas for each curso
+            for ($i = 1; $i <= 5; $i++) {
+                Aulas::factory()->create([
+                    'curso_id' => $curso->id,
+                    'sequencia' => $i, // Ensures sequencia is unique for each curso
+                ]);
+            }
+        }
     }
 }
